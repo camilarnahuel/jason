c0165e3c3ef5ef58203eb54f7160ebdaef2923a3
Camila Nahuel <camilanahuel@MacBook-Air-de-Camila.local>
2023-11-08 12:13:28 -0500

    objetos

--- assests/js/objetos.js
+++ assests/js/objetos.js
@@ -5,9 +5,22 @@ const animal={
     especie:'gato',
     edad:'4',
     comer(){
-        console.log(`${animal.nombre} come`)
+        //console.log(`${animal.nombre} come`)
         console.log(`${this.nombre} come`)
     }
 }
 
-console.log(animal.comer())
+animal.comer();
+
+// DOM (documento object model)
+//document.getElementById('') por id
+//document.querySelector('') #
+//document.querySelectorAll('') [] lista
+console.log(document.querySelectorAll('a').getAttribute('href'))
+document.querySelectorAll('a').setAttribute('href','www.google.com')
+
+/*seleccionar elementos*/
+const $mi = document.querySelector('#clase')
+$mi.computedStyleMap.setProperty('color','#FFFFF')
+
+let text=`<p> bla bla</p>`
